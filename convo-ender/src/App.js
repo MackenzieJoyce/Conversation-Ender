f28397124852cd8363ac470dc1c5b4b1cc844617
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-25 18:22:58 -0400

    build out basic computer structure

--- convo-ender/src/App.js
+++ convo-ender/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id="frame">
+        <section id="display">
+          <section id="screen"></section>
+          <section id="appBar"></section>
+        </section>
+      </section>
+      <section id="stand"></section>
     </div>
   );
 }
