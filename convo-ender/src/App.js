bcb5ae30666a29a744bf869be98ade635e4362ae
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-25 18:54:28 -0400

    added section labels to the styling sheet. added compStand rule

--- convo-ender/src/App.js
+++ convo-ender/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <section id="frame">
-        <section id="display">
+      <section id="compFrame">
+        <section id="compDisplay">
           <section id="screen"></section>
           <section id="appBar"></section>
         </section>
       </section>
-      <section id="stand"></section>
+      <section id="compStand"></section>
     </div>
   );
 }
