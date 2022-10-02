a48c3b2208c6399274829049e6f3807a45afcd6f
Mackenzie <mackenziejoyce414@gmail.com>
2022-10-02 16:32:01 -0400

    created a json file holding prompts and options. after a few thousand tries: set up the im.js to use variables and find the prompt by the array placement... NEXT STEP: need to make it so it finds the hard coded id and not the index

--- convo-ender/src/components/Im.js
+++ convo-ender/src/components/Im.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import promptsAndChoices from '../utils/promptsAndChoices';
+import POA from '../utils/pAO.json';
 
 function Im() {
+  // console.log(POA);
+  // const [currentPrompt, setCurrentPrompt] = useState(POA[0].text);
 
-    
+  const [index, setIndex] = useState(0);
+  const prompt = POA[index].text;
+  const options = POA[index].options
 
   return (
     <section id="IMWindow">
       <section id="titleBar"></section>
-      <section id="conversation"></section>
+      <section id="conversation">{prompt}</section>
+      {/* {currentPrompt} */}
+      {/* {console.log(currentPrompt)} */}
       <section id="userInteractionBox">
-        <section id="choiceButtons"></section>
-        <button id="sendMessage">SEND</button>
+        {options && options.map((option, i) => {
+          return (
+            <>
+              <button key={i}>{option.text}</button>
+            </>
+          )
+        })}
+        {/* {currentPrompt.options &&
+          currentPrompt.options.map(({ text }) => (
+            <button key={currentPrompt.options.id}>{text}</button>
+          ))} */}
+        {/* {console.log(currentPrompt.options)} */}
       </section>
     </section>
   );
